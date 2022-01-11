3a385fce6cc509dd3d29e9bdfa30d0cec6dd5319
thang <thanghn2503@gamil.com>
2022-01-11 19:55:34 +0700

    update

--- main.js
+++ main.js
@@ -16,4 +16,23 @@ for(let i = 0; i < somePackages.length; i++){
     }
 }
 
+var sliderImg = document.querySelectorAll('.highlight_place-slider-img');
+var imgSelected = document.querySelectorAll('.highlight_place-slider-img-selected-item');
+let i = 0;
+setInterval(() => {
+    if(i > 2) i = 0;
+    let a = sliderImg[i];
+    let b = imgSelected[i];
+    a.classList.add('display');
+    b.classList.add('selected');
+    setTimeout(() => {
+        a.classList.remove('display');
+        b.classList.remove('selected');
+    },5000);
+    i++;
+},5000)
+
+
+
+
 
